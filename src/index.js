b07480b14398b4353b27d8afb50eee7317595bd9
zlatanov-dev <zlatanov.yasen@gmail.com>
2023-05-01 12:54:18 +0300

    Configuration of express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const config = require('./config');
+const handlebars = require('express-handlebars');
 
 const app = express();
+app.engine('hbs', handlebars.engine({
+   extname:'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-   res.send('Hello!'); 
+   res.render('home'); 
 });
-
-
-app.get('/cat', (req, res) => {
-   res.send('!!!!!')
-});
-
 
 app.listen(config.PORT, () => console.log(`Server running on port: ${config.PORT}`));
