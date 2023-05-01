098b7966bc579461ea36708652da3afae7c7c125
zlatanov-dev <zlatanov.yasen@gmail.com>
2023-05-01 13:48:02 +0300

    Add routes and controler

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const config = require('./config/configuration');
 const viewEngineSetup = require('./config/viewEngine')
+const cubeController = require('./controllers/cubeControler');
 
 const app = express();
 // Configure Express-Handlebars
 viewEngineSetup(app);
 
 app.use(express.static('./public'));
+
+// Routing
 app.get('/', (req, res) => {
    res.render('index'); 
 });
+
+app.get('/about', (req, res) => {
+   res.render('about'); 
+});
+
+// app.get('/create', (req, res) => {
+//    res.render('create'); 
+// });
+
+app.get('/create', cubeController.getCreateCube);
 
 app.listen(config.PORT, () => console.log(`Server running on port: ${config.PORT}`));
