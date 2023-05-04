b5f5332ba6f25a0538c3f590fb6f841140c06eab
zlatanov-dev <zlatanov.yasen@gmail.com>
2023-05-04 10:22:03 +0300

    Add Cube

--- src/controllers/cubeControler.js
+++ src/controllers/cubeControler.js
@@ -1,14 +1,14 @@
-const Cube = require('../models/Cube_Old');
+const Cube = require('../models/Cube');
 const db = require('../db.json');
 
 exports.getCreateCube = (req, res) => {
     res.render('create'); 
 }
 
-exports.postCreateCube = (req, res) => {
+exports.postCreateCube = async (req, res) => {
     const {name, description, imageUrl, difficultyLevel} = req.body;
-    let cube = new Cube(name, description, imageUrl, difficultyLevel);
-    cube.save();
+    let cube = new Cube({name, description, imageUrl, difficultyLevel});
+    await cube.save();
     res.redirect('/');
 };
 
