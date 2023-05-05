33625d1c79e05e0b0558d3425d2e3616f5e98ed8
zlatanov-dev <zlatanov.yasen@gmail.com>
2023-05-05 11:23:09 +0300

    Add Accessory page

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
+const Accessory = require('../models/Accessory');
 
 // URL: /accessory/create 
 router.get('/create', (req, res) => {
     res.render('accessory/createAccessory');
 });
 
+router.post('/create', async (req, res) => {
+    const {name, description, imageUrl} = req.body
+    await Accessory.create({name, description, imageUrl});
+    res.redirect('/');
+});
+
+router.get('/attach', (req, res) => {
+   res.render('accessory/attach'); 
+});
 module.exports = router;
