ccfc87ea7c35d8916deb7e4a2ba129c4fcfb416c
zlatanov-dev <zlatanov.yasen@gmail.com>
2023-05-09 13:30:00 +0300

    Add Delete page and Edit functionallity

--- src/controllers/cubeControler.js
+++ src/controllers/cubeControler.js
@@ -1,64 +1,81 @@
-const Cube = require('../models/Cube');
-const Accessorry = require('../models/Accessory');
-const cubeService = require('../services/cubeService');
-const cubeUtils = require('../utils/cubeUtils');
-
+const Cube = require("../models/Cube");
+const Accessorry = require("../models/Accessory");
+const cubeService = require("../services/cubeService");
+const cubeUtils = require("../utils/cubeUtils");
 
 exports.getCreateCube = (req, res) => {
-    res.render('create'); 
-}
+  res.render("create");
+};
 
 exports.postCreateCube = async (req, res) => {
-    const {name, description, imageUrl, difficultyLevel} = req.body;
+  const { name, description, imageUrl, difficultyLevel } = req.body;
 
-    try{
-        let cube = new Cube({name, description, imageUrl, difficultyLevel});
-        await cube.save();
-        res.redirect('/');
-    } catch(err) {
-        console.log(err.message);
-        return res.redirect('/404');
-    }
+  try {
+    let cube = new Cube({ name, description, imageUrl, difficultyLevel });
+    await cube.save();
+    res.redirect("/");
+  } catch (err) {
+    console.log(err.message);
+    return res.redirect("/404");
+  }
 };
 
 exports.getDetails = async (req, res) => {
+  try {
+    const cube = await Cube.findById(req.params.cubeId)
+      .populate("accessories")
+      .lean();
 
-    try {
-        const cube = await Cube.findById(req.params.cubeId).populate('accessories').lean();
-
-        if(!cube) {
-            throw new Error('Invalid cube id!');
-        }
-
-        res.render(`cube/details`, { cube });
-    } catch(err) {
-        return res.redirect('/404'); 
+    if (!cube) {
+      throw new Error("Invalid cube id!");
     }
+
+    res.render(`cube/details`, { cube });
+  } catch (err) {
+    return res.redirect("/404");
+  }
 };
 
 exports.getAttachAccessory = async (req, res) => {
-    const cube = await Cube.findById(req.params.cubeId).lean();
-    const accessories = await Accessorry.find({_id: {$nin: cube.accessories}}).lean();
-    res.render('cube/attach', {cube, accessories});
+  const cube = await Cube.findById(req.params.cubeId).lean();
+  const accessories = await Accessorry.find({
+    _id: { $nin: cube.accessories },
+  }).lean();
+  res.render("cube/attach", { cube, accessories });
 };
 
-exports.postAttachAccessory = async (req,res) => {
-    const cube = await Cube.findById(req.params.cubeId);
-    const accessoryId = req.body.accessory;
-    cube.accessories.push(accessoryId)
-    await cube.save();
-    res.redirect(`/cubes/${cube._id}/details`);
+exports.postAttachAccessory = async (req, res) => {
+  const cube = await Cube.findById(req.params.cubeId);
+  const accessoryId = req.body.accessory;
+  cube.accessories.push(accessoryId);
+  await cube.save();
+  res.redirect(`/cubes/${cube._id}/details`);
 };
 
 exports.getEditCube = async (req, res) => {
-    const cube = await cubeService.getOne(req.params.cubeId);
-    const difficultyLevel = cubeUtils.generateDificultyLevels(cube.difficultyLevel);
-    res.render('cube/edit', {cube, difficultyLevel});
+  const cube = await cubeService.getOne(req.params.cubeId);
+  const difficultyLevel = cubeUtils.generateDificultyLevels(
+    cube.difficultyLevel
+  );
+  res.render("cube/edit", { cube, difficultyLevel });
+};
+
+exports.postEditCube = async (req, res) => {
+  const { name, description, imageUrl, difficultyLevel } = req.body;
+  await cubeService.update(req.params.cubeId, {
+    name,
+    description,
+    imageUrl,
+    difficultyLevel,
+  });
+  res.redirect(`/cubes/${req.params.cubeId}/details`);
 };
 
 exports.getDeleteCube = async (req, res) => {
-    const cube = await cubeService.getOne(req.params.cubeId);
-    res.render('cube/delete', {cube});
+  const cube = await cubeService.getOne(req.params.cubeId);
+  const difficultyLevel = cubeUtils.generateDificultyLevels(
+    cube.difficultyLevel
+  );
 
+  res.render("cube/delete", { cube, difficultyLevel });
 };
-
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -3,3 +3,8 @@ const Cube = require('../models/Cube');
 exports.getOne = (cubeId) => {
     return Cube.findById(cubeId).lean();
 }
+
+exports.update = (cubeId, data) => {
+    // Run validate data
+    return Cube.findByIdAndUpdate(cubeId, data, {runValidators: true});
+}
