5307a0325f3dcb24b951ca1539de688884c08fb2
zlatanov-dev <zlatanov.yasen@gmail.com>
2023-05-09 13:31:01 +0300

    Revert "Add Delete page and Edit functionality"
    This reverts commit ccfc87ea7c35d8916deb7e4a2ba129c4fcfb416c.

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -3,8 +3,3 @@ const Cube = require('../models/Cube');
 exports.getOne = (cubeId) => {
     return Cube.findById(cubeId).lean();
 }
-
-exports.update = (cubeId, data) => {
-    // Run validate data
-    return Cube.findByIdAndUpdate(cubeId, data, {runValidators: true});
-}
